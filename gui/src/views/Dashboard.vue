ebca8c884c49e5fb3098ac2cb96321675a043ea6
filip <filda.kuchar@seznam.cz>
2020-11-15 17:24:31 +0100

    finish up dashboard

--- gui/src/views/Dashboard.vue
+++ gui/src/views/Dashboard.vue
@@ -19,11 +19,13 @@
           <select
             aria-label="Selected tab"
             class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5 transition ease-in-out duration-150"
+            v-model="currentTab"
           >
-            <option>Load info</option>
-            <option>CPU info</option>
-            <option>CPU usage</option>
-            <option>RAM usage</option>
+            <option v-for="availableTab in availableTabs"
+                    v-bind:key="availableTab.name"
+                    :selected="currentTab === availableTab.name"
+                    :value="availableTab.name"
+            >{{ availableTab.label }}</option>
           </select>
         </div>
         <div class="hidden sm:block">
