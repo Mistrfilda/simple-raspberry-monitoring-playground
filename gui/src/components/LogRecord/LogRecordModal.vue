f31c7b73b97bafb45ccfa803439c5c7d3dcc30e8
filip <filda.kuchar@seznam.cz>
2020-11-17 23:30:48 +0100

    remove comments from templates

--- gui/src/components/LogRecord/LogRecordModal.vue
+++ gui/src/components/LogRecord/LogRecordModal.vue
@@ -24,16 +24,6 @@
         <!-- This element is to trick the browser into centering the modal contents. -->
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span
         >&#8203;
-        <!--
-        Modal panel, show/hide based on modal state.
-
-        Entering: "ease-out duration-300"
-          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          To: "opacity-100 translate-y-0 sm:scale-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100 translate-y-0 sm:scale-100"
-          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      -->
         <transition
           enter-active-class="ease-out duration-300"
           enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
