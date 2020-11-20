38aa0efb31f15442d46935539990d0a0538c25e1
filip <filda.kuchar@seznam.cz>
2020-11-21 00:49:47 +0100

    Add status check endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -31,24 +31,52 @@ app.use(function (req, res, next) {
 const logController = new LogController(config.appLogFolder);
 const systemInformationController = new SystemInformationController();
 
-app.get('/app1/logs', (req: express.Request, res: express.Response) => { logController.getAppLogs(req, res) });
-app.get('/app1/logs/:logHash', (req: express.Request, res: express.Response) => { logController.getAppLogContents(req, res) });
-app.get('/app1/logs/:logHash/info', (req: express.Request, res: express.Response) => { logController.getFileInfo(req, res) });
-app.get('/app1/logs/:logHash/level/:filtered', (req: express.Request, res: express.Response) => { logController.getAppLogContentsFilterLogLevel(req, res) })
-app.get('/app1/logs/:logHash/levels', (req: express.Request, res: express.Response) => { logController.getAppLogContentsFilterMultipleLogLevels(req, res) })
-app.get('/app1/logs/:logHash/filter/:filtered', (req: express.Request, res: express.Response) => { logController.getAppLogContentsFilterAboveLogLevel(req, res) })
+app.get('/app1/status', (req: express.Request, res: express.Response) => {
+    res.json({status: 'ok'});
+});
 
-app.get('/app1/system/cpuinfo', (req: express.Request, res: express.Response) => { systemInformationController.getCpuInfo(req, res) });
-app.get('/app1/system/cputemp', (req: express.Request, res: express.Response) => { systemInformationController.getCurrentCpuTemperature(req, res) });
-app.get('/app1/system/cpuspeed', (req: express.Request, res: express.Response) => { systemInformationController.getCurrentCpuSpeed(req, res) });
+app.get('/app1/logs', (req: express.Request, res: express.Response) => {
+    logController.getAppLogs(req, res)
+});
+app.get('/app1/logs/:logHash', (req: express.Request, res: express.Response) => {
+    logController.getAppLogContents(req, res)
+});
+app.get('/app1/logs/:logHash/info', (req: express.Request, res: express.Response) => {
+    logController.getFileInfo(req, res)
+});
+app.get('/app1/logs/:logHash/level/:filtered', (req: express.Request, res: express.Response) => {
+    logController.getAppLogContentsFilterLogLevel(req, res)
+})
+app.get('/app1/logs/:logHash/levels', (req: express.Request, res: express.Response) => {
+    logController.getAppLogContentsFilterMultipleLogLevels(req, res)
+})
+app.get('/app1/logs/:logHash/filter/:filtered', (req: express.Request, res: express.Response) => {
+    logController.getAppLogContentsFilterAboveLogLevel(req, res)
+})
 
-app.get('/app1/system/currentLoad', (req: express.Request, res: express.Response) => { systemInformationController.getCurrentLoad(req, res) });
+app.get('/app1/system/cpuinfo', (req: express.Request, res: express.Response) => {
+    systemInformationController.getCpuInfo(req, res)
+});
+app.get('/app1/system/cputemp', (req: express.Request, res: express.Response) => {
+    systemInformationController.getCurrentCpuTemperature(req, res)
+});
+app.get('/app1/system/cpuspeed', (req: express.Request, res: express.Response) => {
+    systemInformationController.getCurrentCpuSpeed(req, res)
+});
 
-app.get('/app1/system/raminfo', (req: express.Request, res: express.Response) => { systemInformationController.getCurrentRamUsage(req, res) });
+app.get('/app1/system/currentLoad', (req: express.Request, res: express.Response) => {
+    systemInformationController.getCurrentLoad(req, res)
+});
 
-app.get('/app1/system/getAll', (req: express.Request, res: express.Response) => { systemInformationController.getAllData(req, res) });
+app.get('/app1/system/raminfo', (req: express.Request, res: express.Response) => {
+    systemInformationController.getCurrentRamUsage(req, res)
+});
+
+app.get('/app1/system/getAll', (req: express.Request, res: express.Response) => {
+    systemInformationController.getAllData(req, res)
+});
 
 // start the Express server
-app.listen(port, function() {
-    console.log(`server started at http://localhost:${ port }` );
+app.listen(port, function () {
+    console.log(`server started at http://localhost:${port}`);
 });
