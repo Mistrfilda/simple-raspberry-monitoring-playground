abf1a3f229e128e361c03bfc744e3a35cf4f4985
filip <filda.kuchar@seznam.cz>
2020-11-22 22:11:29 +0100

    update title, prepare electron icons

--- gui/src/background.ts
+++ gui/src/background.ts
@@ -1,8 +1,11 @@
 "use strict";
 
+/* global __static */
+
 import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import * as path from "path";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
@@ -13,14 +16,15 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1000,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: (process.env
         .ELECTRON_NODE_INTEGRATION as unknown) as boolean
-    }
+    },
+    // icon: path.join(__static, 'icon.png')
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
