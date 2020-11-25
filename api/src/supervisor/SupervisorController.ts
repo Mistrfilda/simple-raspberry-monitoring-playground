b035e2a130a66d841b8ed6b75b163387b224ca14
filip <filda.kuchar@seznam.cz>
2020-11-25 23:49:21 +0100

    clean up supervisor facade, add supervisor controller methods

--- api/src/supervisor/SupervisorController.ts
+++ api/src/supervisor/SupervisorController.ts
@@ -1,6 +1,10 @@
 import {BaseController} from "../controller/BaseController";
 import {SupervisorXmlRpcFacade} from "./xmlRpc/SupervisorXmlRpcFacade";
 import {ConfigDefinition} from "../config/ConfigDefinition";
+import {Request, Response} from "express";
+import {SupervisorVersion} from "./xmlRpc/response/SupervisorVersion";
+import {SupervisorStateInfo} from "./xmlRpc/response/SupervisorStateInfo";
+import {ProcessInfo} from "./xmlRpc/response/ProcessInfo";
 
 export default class SupervisorController extends BaseController {
     supervisorXmlRpcFacade: SupervisorXmlRpcFacade;
@@ -10,4 +14,33 @@ export default class SupervisorController extends BaseController {
         this.supervisorXmlRpcFacade = new SupervisorXmlRpcFacade(config.supervisor.xmlRpc.supervisorXmlRpcBasePath);
     }
 
+    public getSupervisorVersion(req: Request, res: Response) {
+        this.supervisorXmlRpcFacade.getSupervisorVersion()
+            .then(function (data: SupervisorVersion) {
+                res.json(data);
+            })
+            .catch(function (error: string) {
+                throw new Error(error);
+            });
+    }
+
+    public getSupervisorState(req: Request, res: Response) {
+        this.supervisorXmlRpcFacade.getSupervisorState()
+            .then(function (data: SupervisorStateInfo) {
+                res.json(data);
+            })
+            .catch(function (error: string) {
+                throw new Error(error);
+            });
+    }
+
+    public getAllProcesses(req: Request, res: Response) {
+        this.supervisorXmlRpcFacade.getAllProcesses()
+            .then(function (data: ProcessInfo[]) {
+                res.json(data);
+            })
+            .catch(function (error: string) {
+                throw new Error(error);
+            });
+    }
 }
